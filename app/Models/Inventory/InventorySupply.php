b925476a5f95803fcf68dab791c6504e1ac87848
Patrick Noah Policarpio <patrickpolicarpio08@gmail.com>
2024-07-27 22:51:14 +0800

    inventories api - wip

---
+++ app/Models/Inventory/InventorySupply.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Inventory;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InventorySupply extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
---
+++ app/Repositories/Inventory/SuppliesRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repositories\Inventory;
+
+class SuppliesRepository
+{
+    //
+}
