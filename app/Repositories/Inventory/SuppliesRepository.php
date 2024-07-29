6cd09807cf306ec8f0d7f6fe92879301d499dbd0
Patrick Noah Policarpio <patrickpolicarpio08@gmail.com>
2024-07-29 08:08:35 +0800

    supplies service/repository

--- app/Repositories/Inventory/SuppliesRepository.php
+++ app/Repositories/Inventory/SuppliesRepository.php
@@ -2,7 +2,18 @@
 
 namespace App\Repositories\Inventory;
 
-class SuppliesRepository
+use Illuminate\Database\Eloquent\Model;
+use App\Repositories\BaseRepository;
+
+class SuppliesRepository extends BaseRepository
 {
-    //
+    public function __construct(
+        private readonly Model $model,
+        private readonly array $relationships = [],
+        private readonly array $shownRelationshipsInList = []
+
+    )
+    {
+        parent::__construct($model, $relationships, $shownRelationshipsInList);
+    }
 }
--- app/Services/Admin/Inventory/SuppliesService.php
+++ app/Services/Admin/Inventory/SuppliesService.php
@@ -2,7 +2,13 @@
 
 namespace App\Services\Admin\Inventory;
 
-class InventorySuppliesService
+use App\Models\Inventory\InventorySupply;
+use App\Repositories\Inventory\SuppliesRepository;
+
+class InventorySuppliesService extends SuppliesRepository
 {
-    //
+    public function __construct(InventorySupply $model)
+    {
+        parent::__construct($model, ['users'], []);
+    }
 }
